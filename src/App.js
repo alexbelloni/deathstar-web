85839349d9b114b87ae10da06adcd0ff53fba06d
alexbelloni <alexbelloni@outlook.com>
2021-03-22 22:12:12 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import './App.css';
+import { useState } from "react";
+import Profile from './pages/Profile';
+require('dotenv').config()
+
+function App() {
+  const [loggedUser, setLoggedUser] = useState();
+  const [error, setError] = useState();
+  const [user, setUser] = useState({ email: "", password: "" });
+
+  const handleClick = e => {
+    if (e) e.preventDefault();
+    login({
+      email: user.email,
+      password: user.password
+    });
+  }
+
+  function login(data) {
+    fetch(`${process.env.REACT_APP_BASE_URL}login`, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    })
+      .then(res => res.json())
+      .then(res => {
+        if (res.error) {
+          setError(res.error);
+        } else {
+          setLoggedUser(res);
+        }
+      })
+      .catch(e => setError(e.message));
+  }
+
+  const signOut = () => {
+    setError('');
+    setUser({ email: "", password: "" });
+    setLoggedUser(null);
+  }
+
+  const handleChange = e => {
+    const newUser = { ...user, [e.target.name]: e.target.value };
+    setUser(newUser);
+  }
+
+  const handleTrial = e => {
+    login({
+      "email": process.env.REACT_APP_TRIAL_EMAIL, 
+      "password": process.env.REACT_APP_TRIAL_PASSWORD
+    })
+  }
+
+  return loggedUser ? <Profile user={loggedUser} signOut={signOut} />
+    : (
+      <div className="container">
+        <div id="login">
+          <form>
+            <fieldset className="clearfix">
+              <p>
+                <span className="fontawesome-envelope"></span>
+                <input type="text" name='email' value={user.email} onChange={handleChange}
+                  placeholder='e-mail' required />
+              </p>
+              <p><span className="fontawesome-lock"></span>
+                <input type="password" name='password' value={user.password} onChange={handleChange}
+                  placeholder='password' required />
+              </p>
+              <p>
+                <input type="submit" value="Sign In" onClick={handleClick} />
+              </p>
+            </fieldset>
+          </form>
+          <footer>
+            <p>Not a member? <a href="#">Sign up now</a><span className="fontawesome-arrow-right"></span></p>
+            <p onClick={handleTrial}><a href="#">Free Trial</a><span className="fontawesome-arrow-right"></span></p>
+            {error && <p>{error.toUpperCase()}</p>}
+          </footer>
+        </div>
+      </div>
+    );
+}
+
+export default App;
---
+++ src/pages/Profile.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import './Profile.css';
+
+const Profile = (props) => {
+    const [users, setUsers] = useState();
+
+    useEffect(() => {
+        if (!props.user.token) return
+
+        fetch(`${process.env.REACT_APP_BASE_URL}users`, {
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `JWT ${props.user.token}`
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: null
+        })
+            .then(res => res.json())
+            .then(res => {
+                setUsers(res)
+            })
+            .catch(e => console.log(e));
+    }, []);
+
+    const handleClick = () => {
+        props.signOut();
+    }
+
+    return (
+        <div className="container">
+            <div className="profile">
+                <img className="avatar" alt='avatar' src={props.user.avatar} />
+                <div className="info">
+                    <span className="username">{props.user.name}</span>
+                    <span className="email">{props.user.email}</span>
+                    <span className="email">{props.user.country}</span>
+                </div>
+            </div>
+            <div className="separator"></div>
+            <div className="users">
+                <p>Other Users:</p>
+                {users && users.filter(u => u.email !== props.user.email).sort((a, b) => a.name - b.name).map((u, i) => (
+                    <div key={i} className="user">
+                        <div className="info-user">
+                            <span className="username">{u.name}</span>
+                            <span className="email">{u.country || "-"}</span>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="separator"></div>
+            <p onClick={handleClick}><a href="#">Sign out</a><span className="fontawesome-arrow-right"></span></p>
+        </div>
+    )
+}
+
+export default Profile;
